396de90817e739928f6518d811d00ff3e3cf692c
Hồ Toàn <hotoancntt15a@hotmail.com>
2024-07-23 17:00:11 +0700

    Change

--- ToolBaoCao/Controllers/BaoCaoController.cs
+++ ToolBaoCao/Controllers/BaoCaoController.cs
@@ -39,7 +39,7 @@ namespace ToolBaoCao.Controllers
                 string pathFileTemplate = Server.MapPath("~/App_Data/baocaotuan.docx");
                 if (System.IO.File.Exists(pathFileTemplate) == false)
                 {
-                    ViewBag.Error = "Không tìm thấy tập tin mẫu báo cáo 'baocaotuan.docx' trong thư mục App_Data"; 
+                    ViewBag.Error = "Không tìm thấy tập tin mẫu báo cáo 'baocaotuan.docx' trong thư mục App_Data";
                     return View();
                 }
                 string matinh = Request.getValue("matinh");
@@ -60,25 +60,61 @@ namespace ToolBaoCao.Controllers
                 * X71 = {cột S T_BHTT_NOI bảng B02_TOANQUOC }
                 * X72={cột T T_BHTT_NGOAI bảng B02_TOANQUOC }
                 */
-                var data = AppHelper.dbSqliteWork.getDataTable($@"SELECT IFNULL(p1.t_bhtt, 0) AS t, IFNULL(p1.t_bhtt_noi, 0) AS noi, IFNULL(p1.t_bhtt_ngoai, 0) AS ngoai FROM b02chitiet p1 INNER JOIN b02 ON p1.id2=b02.id WHERE b02.tu_thang={thang} AND b02.den_thang={thang} AND b02.nam={nam} AND b02.cs='1' AND p1.ma_tinh='{matinh.sqliteGetValueField()}' LIMIT 1");
+                var data = AppHelper.dbSqliteWork.getDataTable($@"SELECT IFNULL(p1.t_bhtt, 0) AS x1, IFNULL(p1.t_bhtt_noi, 0) AS x71, IFNULL(p1.t_bhtt_ngoai, 0) AS x72
+                    , IFNULL(p1.tyle_noitru, 0) AS x5
+                    FROM b02chitiet p1 INNER JOIN b02 ON p1.id2=b02.id
+                    WHERE b02.tu_thang={thang} AND b02.den_thang={thang} AND b02.nam={nam} AND b02.cs='1' AND p1.ma_tinh='{matinh.sqliteGetValueField()}' LIMIT 1");
+
                 if (data.Rows.Count > 0)
                 {
-                    tailieu.Add("{X1}", data.Rows[1].ToString());
-                    tailieu.Add("{X71}", data.Rows[2].ToString());
-                    tailieu.Add("{X72}", data.Rows[3].ToString());
+                    tailieu.Add("{X1}", data.Rows[0].ToString());
+                    tailieu.Add("{X71}", data.Rows[1].ToString());
+                    tailieu.Add("{X72}", data.Rows[2].ToString());
+                    tailieu.Add("{X5}", data.Rows[3].ToString());
                 }
                 else
                 {
                     tailieu.Add("{X1}", "0");
                     tailieu.Add("{X71}", "0");
                     tailieu.Add("{X72}", "0");
+                    tailieu.Add("{X5}", "0");
                 }
                 /*
                  X2={“ Quyết định số: Nếu không tìm thấy dòng nào của năm 2024 ở bảng hệ thống lưu thông tin quyết định giao dự toán thì “TW chưa giao dự toán, tạm lấy theo dự toán năm trước”, nếu thấy lấy số ký hiệu các dòng QĐ của năm 2024 ở bảng hệ thống lưu thông tin quyết định giao dự toán}
                  X3={Như trên, ko thấy thì lấy tổng tiền các dòng dự toán năm trước, thấy thì lấy tổng số tiền các dòng quyết định năm nay}
                  */
-                tailieu.Add("{X2}", "0");
-                tailieu.Add("{X3}", "0");
+                tailieu.Add("{X2}", "0"); /* Nhập từ bảng dữ liệu */
+                tailieu.Add("{X3}", "0"); /* Nhập từ bảng dữ liệu */
+                /* B02 */
+                /*
+                 * X5={Cột G, dòng MA_TINH=10};
+                 * X6={cột G, dòng MA_TINH=00};
+                 * X7={đoạn văn tùy thuộc X5> hay < X6. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số };
+                 * X8={Sort cột G (TYLE_NOITRU) cao xuống thấp và lấy thứ tự};
+                 * X9 ={tính toán: total cột F (TONG_LUOT_NOI) chia cho Total cột D (TONG_LUOT) của các tỉnh có MA_VUNG=mã vùng của tỉnh báo cáo};
+                 * X10 ={đoạn văn tùy thuộc X5> hay < X9. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số };
+                 * X11= {lọc các dòng tỉnh có mã vùng trùng với mã vùng của tỉnh, sort cột G (TYLE_NOITRU ) cao –thấp và lấy thứ tự}
+                 * */
+                data = AppHelper.dbSqliteWork.getDataTable($@"SELECT IFNULL(p1.tyle_noitru, 0) AS x6
+                    FROM b02chitiet p1 INNER JOIN b02 ON p1.id2=b02.id
+                    WHERE b02.tu_thang={thang} AND b02.den_thang={thang} AND b02.nam={nam} AND b02.cs='1' AND p1.ma_tinh='00' LIMIT 1");
+
+                if (data.Rows.Count > 0)
+                {
+                    tailieu.Add("{X6}", data.Rows[0].ToString());
+                }
+                else
+                {
+                    tailieu.Add("{X6}", "0");
+                }
+                if (tailieu["{X5}"] == tailieu["{X6}"]) { tailieu["{X7}"] = "Bằng"; }
+                else
+                {
+                    if (double.Parse(tailieu["{X5}"]) > double.Parse(tailieu["{X6}"])) { tailieu["{X7}"] = "Cao hơn"; }
+                    else { tailieu["{X7}"] = "Thấp hơn"; }
+                }
+                if (tailieu["{X3}"] == "0") { tailieu.Add("{X4}", "0"); }
+                else { tailieu.Add("{X4}", (double.Parse(tailieu["{X1}"])/double.Parse(tailieu["{X3}"])).ToString("0.###")); }
 
 
                 using (var fileStream = new FileStream(pathFileTemplate, FileMode.Open, FileAccess.ReadWrite))
@@ -91,11 +127,11 @@ namespace ToolBaoCao.Controllers
                             tmp = run.ToString();
                             foreach (var v in tailieu)
                             {
-                                if (tmp.Contains(v.Key)) { tmp = tmp.Replace(v.Key, v.Value); }
+                                if (tmp.Contains(v.Key)) { tmp = tmp.Replace(v.Key, v.Value.Replace(".", ",")); }
                             }
                             /* Xóa hết các thông tin {X[0-9]+} nếu còn */
                             tmp = Regex.Replace(tmp, "{X[0-9]+}", "", RegexOptions.IgnoreCase);
-                            run.SetText(tmp,0);
+                            run.SetText(tmp, 0);
                         }
                     }
                     MemoryStream memoryStream = new MemoryStream();
