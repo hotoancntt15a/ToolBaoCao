64e91037c000d8d8861053b0cb6878fc3a27e4c3
Hồ Toàn <hotoancntt15a@hotmail.com>
2024-08-14 16:04:13 +0700

    Cập nhập /Admin/dmTinh/

--- ToolBaoCao/Areas/Admin/Controllers/DMCSKCBController.cs
+++ ToolBaoCao/Areas/Admin/Controllers/DMCSKCBController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +12,26 @@ namespace ToolBaoCao.Areas.Admin.Controllers
         // GET: Admin/DMCSKCB
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            var timeStart = DateTime.Now;
+            var mode = Request.getValue("mode");
+            var id = Request.getValue("id");
+            if (Regex.IsMatch(id, @"^[0-9a-z,]+$", RegexOptions.IgnoreCase) == false) { return Content($"Mã không đúng '{id}'".BootstrapAlter("warning")); }
+            try
+            {
+                if (mode == "force")
+                {
+                    var listID = id.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int i = 0; i < listID.Length; i++) { listID[i] = $"'{listID[i].sqliteGetValueField()}'"; }
+                    AppHelper.dbSqliteMain.Execute($"DELETE FROM dmcskcb WHERE id IN ({string.Join(",", listID)});");
+                    return Content($"Xoá thành công ({timeStart.getTimeRun()})".BootstrapAlter());
+                }
+            }
+            catch (Exception ex) { return Content(ex.getErrorSave().BootstrapAlter("warning")); }
+            ViewBag.id = id;
             return View();
         }
     }
--- ToolBaoCao/Areas/Admin/Controllers/DMTinhController.cs
+++ ToolBaoCao/Areas/Admin/Controllers/DMTinhController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
@@ -12,7 +13,7 @@ namespace ToolBaoCao.Areas.Admin.Controllers
         {
             try
             {
-                ViewBag.data = AppHelper.dbSqliteMain.getDataTable("SELECT * FROM dmtinh ORDER BY tt");
+                ViewBag.data = AppHelper.dbSqliteMain.getDataTable("SELECT * FROM dmtinh ORDER BY tt, ten");
             }
             catch (Exception ex) { ViewBag.Error = ex.getErrorSave().BootstrapAlter("warning"); }
             return View();
@@ -24,12 +25,12 @@ namespace ToolBaoCao.Areas.Admin.Controllers
             try
             {
                 var mode = Request.getValue("mode");
-                var id = Request.getValue("id");
+                var id = Request.getValue("objectid");
                 ViewBag.id = id;
                 if (mode == "update")
                 {
                     var item = new Dictionary<string, string>() {
-                        { "id", id },
+                        { "id", Request.getValue("id") },
                         { "ten", Request.getValue("ten") },
                         { "tt", Request.getValue("tt") },
                         { "ghichu", Request.getValue("ghichu") }
@@ -38,15 +39,18 @@ namespace ToolBaoCao.Areas.Admin.Controllers
                     if (item["id"] == "") { return Content("Mã bỏ trống".BootstrapAlter("warning")); }
                     if (item["ten"] == "") { return Content("Tên bỏ trống".BootstrapAlter("warning")); }
                     if (Regex.IsMatch(item["id"], @"^[0-9a-z]+$", RegexOptions.IgnoreCase) == false) { return Content($"Mã không đúng '{id}'".BootstrapAlter("warning")); }
+                    if(id != "") { if(id != item["id"]) { return Content($"Mã '{id}' sửa chữa không khớp '{item["id"]}'"); } }
                     AppHelper.dbSqliteMain.Update("dmtinh", item, "replace");
                     return Content($"Lưu thành công ({timeStart.getTimeRun()})".BootstrapAlter());
                 }
                 if (id != "")
                 {
                     if (Regex.IsMatch(id, @"^[0-9a-z]+$", RegexOptions.IgnoreCase) == false) { return Content($"Mã không đúng '{id}'".BootstrapAlter("warning")); }
-                    var data = AppHelper.dbSqliteMain.getDataTable($"SELECT * FROM dmtinh WHERE idtinh='{id}'");
+                    var data = AppHelper.dbSqliteMain.getDataTable($"SELECT * FROM dmtinh WHERE id='{id}'");
                     if(data.Rows.Count == 0) { return Content($"Tỉnh có mã '{id}' không tồn tại hoặc bị xoá khỏi hệ thống".BootstrapAlter("danger")); }
-                    ViewBag.data = data;
+                    var item = new Dictionary<string, object>();
+                    foreach (DataColumn c in data.Columns) { item.Add(c.ColumnName, data.Rows[0][c.ColumnName]); }
+                    ViewBag.data = item;
                 }
             }
             catch (Exception ex) { return Content(ex.getErrorSave().BootstrapAlter("warning")); }
