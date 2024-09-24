97eca6f456e38f1d60823d3dc151236d47b939f0
Hồ Toàn <hotoancntt15a@hotmail.com>
2024-09-24 14:56:43 +0700

    cập nhật xây dựng bcThang

--- ToolBaoCao/BuildDatabase.cs
+++ ToolBaoCao/BuildDatabase.cs
@@ -558,86 +558,20 @@ namespace ToolBaoCao
             if (tables == null) { tables = dbConnect.getAllTables(); }
             var tsqlCreate = new List<string>();
             /* BaoCaoTuanDocx */
-            tsqlCreate.Add(@"CREATE TABLE IF NOT EXISTS bcthangdocx (
-                    id text not null primary key /* Mã hóa rút gọn và gợi nhớ cho mỗi lần lập BC tuần Dùng trình bày danh sách báo cáo đã lập để tiện cho chọn và xử lý thao tác: Khóa và mở khóa báo cáo/ xóa báo cáo/ xem/in lại */
-                    ,x1 real not null default 0 /* Tổng tiền các CSKCB đã đề nghị bảo hiểm thanh toán (T_BHTT): X1={cột R (T-BHTT) bảng B02_TOANQUOC }. Làm tròn đến triệu đồng */
-                    ,x2 text not null default '' /* Số của Quyết định giao dự toán: X2={“ Nếu không tìm thấy dòng nào của năm 2024 ở bảng hệ thống lưu thông tin quyết định giao dự toán thì “TW chưa giao dự toán, tạm lấy theo dự toán năm trước”, nếu thấy thì  lấy số ký hiệu các dòng QĐ của năm 2024 ở bảng hệ thống lưu thông tin quyết định giao dự toán} */
-                    ,x3 real not null default 0 /* X3={Như trên, ko thấy thì lấy tổng tiền các dòng dự toán năm trước, thấy thì lấy tổng số tiền các dòng quyết định năm nay} */
-                    ,x4 real not null default 0 /* So sánh với dự toán, tỉnh đã sử dụng X4={X1/X2%} */
-                    ,x5 real not null default 0 /* Tỷ lệ điều trị nội trú X5={Cột G, dòng MA_TINH=10}; */
-                    ,x6 real not null default 0 /* bình quân toàn quốc X6={cột G, dòng MA_TINH=00}; */
-                    ,x7 text not null default '' /* Số chênh lệch X7={đoạn văn tùy thuộc X5> hay < X6. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x8 integer not null default 0 /* xếp thứ so với các tỉnh X8={Sort cột G (TYLE_NOITRU ) cao xuống thấp và lấy thứ tự}; */
-                    ,x9 real not null default 0 /* Bình quân vùng X9 ={tính toán: total cột F (TONG_LUOT_NOI) chia cho Total cột D (TONG_LUOT) của các tỉnh có MA_VUNG=mã vùng của tỉnh báo cáo}; */
-                    ,x10 text not null default '' /* Số chênh lệch X10 ={đoạn văn tùy thuộc X5> hay < X9. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x11 integer not null default 0 /* đứng thứ so với vùng. X11= {lọc các dòng tỉnh có mã vùng trùng với mã vùng của tỉnh, sort cột G (TYLE_NOITRU ) cao –thấp và lấy thứ tự} */
-                    ,x12 real not null default 0 /* Ngày điều trị bình quân X12={Cột H, dòng MA_TINH=10}; */
-                    ,x13 real not null default 0 /* bình quân toàn quốc X13={cột H, dòng MA_TINH=00}; */
-                    ,x14 text not null default '' /* Số chênh lệch X14={đoạn văn tùy thuộc X12> hay < X13. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x15 integer not null default 0 /* xếp thứ so toàn quốc X15={Sort cột H (NGAY_DTRI_BQ) cao xuống thấp và lấy thứ tự}; */
-                    ,x16 real not null default 0 /* Bình quân vùng X16 ={tính toán: A-Tổng ngày điều trị nội trú các tỉnh cùng mã vùng / B- Tổng lượt kcb nội trú của cá tỉnh cùng mã vùng. A=Total(cột H (NGAY_DTRI_BQ) * cột F (TONG_LUOT_NOI)) của tất cả các tỉnh cùng MA_VUNG với tỉnh báo cáo. B= Total cột F (TONG_LUOT_NOI) của các tỉnh có MA_VUNG cùng mã vùng của tỉnh báo cáo}; */
-                    ,x17 text not null default '' /* Số chênh lệch X17 ={đoạn văn tùy thuộc X12> hay < X16. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x18 integer not null default 0 /* đứng thứ so với vùng X18= {lọc các dòng tỉnh có mã vùng trùng với mã vùng của tỉnh, sort Cột H (NGAY_DTRI_BQ) cao –thấp và lấy thứ tự} */
-                    ,x19 real not null default 0 /* Chi bình quân chung X19={Cột I (CHI_BQ_CHUNG), dòng MA_TINH=10}; */
-                    ,x20 real not null default 0 /* bình quân toàn quốc X20={cột I, dòng MA_TINH=00}; */
-                    ,x21 text not null default '' /* Số chênh lệch X21={đoạn văn tùy thuộc X19> hay < X20. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x22 integer not null default 0 /* xếp thứ so toàn quốc X22={Sort cột I cao xuống thấp và lấy thứ tự}; */
-                    ,x23 real not null default 0 /* Bình quân vùng X23={tính toán: A-Tổng chi các tỉnh cùng mã vùng / B- Tổng lượt kcb của các tỉnh cùng mã vùng. A=Total  (cột I (CHI_BQ_CHUNG) * cột D (TONG_LUOT)) của tất cả các tỉnh cùng MA_VUNG với tỉnh báo cáo. B= Total cột D (TONG_LUOT) của các tỉnh có MA_VUNG cùng mã vùng của tỉnh báo cáo}; */
-                    ,x24 text not null default '' /* Số chênh lệch X24 ={đoạn văn tùy thuộc X19> hay < X23. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x25 integer not null default 0 /* đứng thứ so với vùng X25= {lọc các dòng tỉnh có mã vùng trùng với mã vùng của tỉnh, sort Cột I (CHI_BQ_CHUNG) cao –thấp và lấy thứ tự} */
-                    ,x26 real not null default 0 /* Chi bình quân ngoại trú X26={Cột J (CHI_BQ_NGOAI), dòng MA_TINH=10}; */
-                    ,x27 real not null default 0 /* bình quân toàn quốc X27={cột J, dòng MA_TINH=00}; */
-                    ,x28 text not null default '' /* Số chênh lệch X28={đoạn văn tùy thuộc X26> hay < X27. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x29 integer not null default 0 /* xếp thứ so toàn quốc X29={Sort cột J cao xuống thấp và lấy thứ tự}; */
-                    ,x30 real not null default 0 /* Bình quân vùng X30={tính toán: A-Tổng chi ngoại trú các tỉnh cùng mã vùng / B- Tổng lượt kcb ngoại trú của các tỉnh cùng mã vùng. A=Total  (cột J (CHI_BQ_NGOAI) * cột E (TONG_LUOT_NGOAI)) của tất cả các tỉnh cùng MA_VUNG với tỉnh báo cáo. B= Total cột E (TONG_LUOT_NGOAI) của các tỉnh có MA_VUNG cùng mã vùng của tỉnh báo cáo}; */
-                    ,x31 text not null default '' /* Số chênh lệch X31 ={đoạn văn tùy thuộc X19> hay < X30. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x32 integer not null default 0 /* đứng thứ so với vùng X32= {lọc các dòng tỉnh có mã vùng trùng với mã vùng của tỉnh, sort Cột J (CHI_BQ_NGOAI) cao –thấp và lấy thứ tự} */
-                    ,x33 real not null default 0 /* Chi bình quân nội trú X33={Cột K (CHI_BQ_NOI), dòng MA_TINH=10}; */
-                    ,x34 real not null default 0 /* bình quân toàn quốc X34={cột K, dòng MA_TINH=00}; */
-                    ,x35 text not null default '' /* Số chênh lệch X35={đoạn văn tùy thuộc X33> hay < X34. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x36 integer not null default 0 /* xếp thứ so toàn quốc X36={Sort cột K cao xuống thấp và lấy thứ tự}; */
-                    ,x37 real not null default 0 /* Bình quân vùng X37={tính toán: A-Tổng chi nội trú các tỉnh cùng mã vùng / B- Tổng lượt kcb nội trú của các tỉnh cùng mã vùng. A=Total  (cột K (CHI_BQ_NOI) * cột F (TONG_LUOT_NOI)) của tất cả các tỉnh cùng MA_VUNG với tỉnh báo cáo. B= Total cột F (TONG_LUOT_NOI) của các tỉnh có MA_VUNG cùng mã vùng của tỉnh báo cáo}; */
-                    ,x38 text not null default '' /* Số chênh lệch X38 ={đoạn văn tùy thuộc X33> hay < X34. Nếu lớn hơn, lấy chuỗi “cao hơn”, không thì “thấp hơn” ghép với trị tuyệt đối của hiệu số }; */
-                    ,x39 integer not null default 0 /* đứng thứ so với vùng X39= {lọc các dòng tỉnh có mã vùng trùng với mã vùng của tỉnh, sort Cột K (CHI_BQ_NOI) cao –thấp và lấy thứ tự} */
-                    ,x40 real not null default 0 /* Bình quân xét nghiệm X40= {cột P dòng có mã tỉnh =10}; */
-                    ,x41 real not null default 0 /* số tương đối so kỳ trước X41={nếu cột Q dòng có mã tỉnh=10 là số dương, “tăng “ & cột Q & “%”, không thì “giảm “ & cột Q %}; */
-                    ,x42 text not null default '' /* số tuyệt đối  so kỳ trước X42={nếu cột Q là dương, “tăng “ & cột P trừ đi (cột P chia (cột Q +100) *100 ) & “ đồng”, không thì “giảm “ &  cột P trừ đi (cột P chia (cột Q +100) *100 ) & “ đồng”}; */
-                    ,x43 real not null default 0 /* Bình quân CĐHA X43= {cột R dòng có mã tỉnh =10}; */
-                    ,x44 text not null default '' /* số tương đối X44={nếu cột S dòng có mã tỉnh=10 là số dương, “tăng “ & cột S & “%”, không thì “giảm “ & cột S %}; */
-                    ,x45 text not null default '' /* số tuyệt đối X45={nếu cột S là dương, “tăng “ & cột R trừ đi (cột R chia (cột S +100) *100 ) & “ đồng”, không thì “giảm “ &  cột R trừ đi (cột R chia (cột S +100) *100 ) & “ đồng”}; */
-                    ,x46 real not null default 0 /* Bình quân thuốc X46= {cột T dòng có mã tỉnh =10}; */
-                    ,x47 text not null default '' /* số tương đối X47={nếu cột U dòng có mã tỉnh=10 là số dương, “tăng “ & cột U & “%”, không thì “giảm “ & cột U %}; */
-                    ,x48 text not null default '' /* số tuyệt đối X48={nếu cột U là dương, “tăng “ & cột T trừ đi (cột T chia (cột U +100) *100 ) & “ đồng”, không thì “giảm “ &  cột T trừ đi (cột T chia (cột U+100) *100 ) & “ đồng”} */
-                    ,x49 real not null default 0 /* Bình quân chi phẫu thuật X49= {cột V dòng có mã tỉnh =10}; */
-                    ,x50 text not null default '' /* số tương đối X50={nếu cột W dòng có mã tỉnh=10 là số dương, “tăng “ & cột W & “%”, không thì “giảm “ & cột W %}; */
-                    ,x51 text not null default '' /* số tuyệt đối X51={nếu cột W là dương, “tăng “ & cột V trừ đi (cột V chia (cột W +100) *100 ) & “ đồng”, không thì “giảm “ &  cột V trừ đi (cột V chia (cột W+100) *100 ) & “ đồng”} */
-                    ,x52 real not null default 0 /* Bình quân chi thủ thuật X52= {cột X dòng có mã tỉnh =10}; */
-                    ,x53 text not null default '' /* số tương đối X53={nếu cột Y dòng có mã tỉnh=10 là số dương, “tăng “ & cột Y & “%”, không thì “giảm “ & cột Y %}; */
-                    ,x54 text not null default '' /* số tuyệt đối X54={nếu cột Y là dương, “tăng “ & cột X trừ đi (cột X chia (cột Y +100) *100 ) & “ đồng”, không thì “giảm “ &  cột X trừ đi (cột X chia (cột Y+100) *100 ) & “ đồng”} */
-                    ,x55 real not null default 0 /* Bình quân chi vật tư y tế X55= {cột Z dòng có mã tỉnh =10}; */
-                    ,x56 text not null default '' /* số tương đối X56={nếu cột AA dòng có mã tỉnh=10 là số dương, “tăng “ & cột AA & “%”, không thì “giảm “ & cột AA %}; */
-                    ,x57 text not null default '' /* số tuyệt đối X57={nếu cột AA là dương, “tăng “ & cột Z trừ đi (cột Z chia (cột AA +100) *100 ) & “ đồng”, không thì “giảm “ &  cột Z trừ đi (cột Z chia (cột AA+100) *100 ) & “ đồng”} */
-                    ,x58 real not null default 0 /* Bình quân chi tiền giường X58= {cột AB dòng có mã tỉnh =10}; */
-                    ,x59 text not null default '' /* số tương đối X59={nếu cột AC dòng có mã tỉnh=10 là số dương, “tăng “ & cột AC & “%”, không thì “giảm “ & cột AC %}; */
-                    ,x60 text not null default '' /* số tuyệt đối X60={nếu cột AC là dương, “tăng “ & cột AB trừ đi (cột AB chia (cột AC +100) *100 ) & “ đồng”, không thì “giảm “ &  cột AB trừ đi (cột AB chia (cột AC+100) *100 ) & “ đồng”} */
-                    ,x61 real not null default 0 /* Chỉ định xét nghiệm X61={cột AD, dòng có mã tỉnh =10 nhân với 100 để ra số người}; */
-                    ,x62 text not null default '' /* số tương đối X62={cột AE dòng có mã tỉnh=10 & “%”}; */
-                    ,x63 text not null default '' /* số tuyệt đối X63 {tính toán: X61 trừ đi (X61 chia (cột AE+100)*100) & “bệnh nhân”}  */
-                    ,x64 real not null default 0 /* Chỉ định CĐHA X64={cột AF, dòng có mã tỉnh =10 nhân với 100 để ra số người}; */
-                    ,x65 text not null default '' /* số tương đối X65={cột AG dòng có mã tỉnh=10 & “%”}; */
-                    ,x66 text not null default '' /* số tuyệt đối X66 {tính toán: X64 trừ đi (X64 chia (cột AG+100)*100) & “bệnh nhân”} */
-                    ,x67 text not null default '' /* Công tác kiểm soát chi X67={lần đầu lập BC sẽ rỗng, người dùng tự trình bày văn bản, lưu lại ở bảng dữ liệu kết quả báo cáo, kỳ sau sẽ tự động lấy từ kỳ trước, để người dùng kế thừa, sửa và lưu dùng cho kỳ này và kỳ sau} */
-                    ,x68 text not null default '' /* Công tác thanh, quyết toán năm X68={tương tự X67} */
-                    ,x69 text not null default '' /* Phương hướng kỳ tiếp theo X69={tương tự X67} */
-                    ,x70 text not null default '' /* Khó khăn, vướng mắc, đề xuất (nếu có) X70={tương tự X67} */
-                    ,x71 real not null default 0 /* Trong đó: Nội trú X71 = {cột S T_BHTT_NOI bảng B02_TOANQUOC }; */
-                    ,x72 real not null default 0 /* Ngoại trú X72={cột T T_BHTT_NGOAI bảng B02_TOANQUOC } */
-                    ,x73 text not null default '' /* Tên tỉnh/thành phố lập BC Lấy biến hệ thống khởi tạo khi User đăng nhập */
-                    ,x74 text not null default '' /* THOI_GIAN_BC Chuỗi ký tự ngày lập BC. mặc định từ ô C3 biểu B26 khi khởi tạo 1 báo cáo	Có ô cho nhập, sửa */
-                    ,userid text not null default '' /* Lưu ID của người dùng */
-                    ,ma_tinh text not null default '' /* Lưu mã tỉnh làm báo cáo */
-                    ,ngay integer not null default 0 /* Ngày làm báo cáo dạng timestamp */
-                    ,timecreate integer not null default 0 /* Thời điểm tạo báo cáo */);");
+            tsqlCreate.Add(@"CREATE TABLE IF NOT EXISTS bcthangdocx (id text not null primary key
+                ,tentinh text not null default '' /* Tên tỉnh */
+                ,ngay text not null default '' /* Ngày báo cáo (Chứa luôn cả ngày đầu tháng, và năm) */
+                ,ngaydauthang text not null '' /* Ngày đầu tháng */
+                ,nam1 integer not null default 0 /* Năm báo cáo */
+                ,nam2 integer not null default 0 /* Năm trước báo cáo */
+                ,mi11 text not null default '' /* Công tác ký hợp đồng KCB BHYT */
+                ,mi12c1 real not null default '' /* Dự toán giao {nam} */
+                ,mi12c2 real not null default '' /* Chi KCB toàn tỉnh */
+                ,mi12c3 real not null default 0 /* Tỷ lệ % SD dự toán {nam} */
+                ,userid text not null default '' /* Lưu ID của người dùng */
+                ,ma_tinh text not null default '' /* Lưu mã tỉnh làm báo cáo */
+                ,timespan integer not null default 0 /* Ngày làm báo cáo dạng timestamp */
+                ,timecreate integer not null default 0 /* Thời điểm tạo báo cáo */);");
             tsqlCreate.Add("CREATE INDEX IF NOT EXISTS bcthangdocx_ma_tinh ON bcthangdocx(ma_tinh);");
             tsqlCreate.Add("CREATE INDEX IF NOT EXISTS index_bcthangdocx_timecreate ON bcthangdocx(timecreate);");
             tsqlCreate.Add("CREATE INDEX IF NOT EXISTS index_bcthangdocx_ngay ON bcthangdocx(ngay);");
