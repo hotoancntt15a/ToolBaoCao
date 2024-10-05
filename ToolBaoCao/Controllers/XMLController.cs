5e5e5ce3ad5749fb7286989221c1944d31403680
Hồ Toàn <hotoancntt15a@hotmail.com>
2024-10-05 08:45:19 +0700

    xml => Thêm tw mực tạm kiểm duyệt các file nén đẩy lên

--- ToolBaoCao/Controllers/XMLController.cs
+++ ToolBaoCao/Controllers/XMLController.cs
@@ -13,6 +13,11 @@ namespace ToolBaoCao.Controllers
             {
                 var d = new DirectoryInfo(Path.Combine(AppHelper.pathAppData, "xml"));
                 if (d.Exists == false) { d.Create(); }
+
+                d = new DirectoryInfo(Path.Combine(AppHelper.pathTemp, "xml"));
+                if (d.Exists == false) { d.Create(); }
+
+                if ($"{Session["idtinh"]}" == "") { ViewBag.Error = "Bạn chưa cấp Mã tỉnh làm việc"; return View(); }
             }
             catch (Exception ex) { ViewBag.Error = ex.getLineHTML(); }
             return View();
