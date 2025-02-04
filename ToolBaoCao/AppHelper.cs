2879abcc3343726c556d899cfbf6de45fd83a4c8
Hồ Toàn <hotoancntt15a@hotmail.com>
2025-02-04 10:20:59 +0700

    Thêm Get SHA1Hash

--- ToolBaoCao/AppHelper.cs
+++ ToolBaoCao/AppHelper.cs
@@ -663,6 +663,18 @@ namespace ToolBaoCao
             }
         }
 
+        public static string GetSHA1Hash(this string input)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha1.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes) { sb.Append(b.ToString("x2")); }
+                return sb.ToString();
+            }
+        }
+
         public static string MD5Encrypt(this string planText)
         {
             if (string.IsNullOrEmpty(planText)) return "";
